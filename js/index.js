2ddee8e8a4b0cba6f3446476dc792e8b1d909dd2
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-05-19 01:11:19 +0500

    project

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// Number
+// console.log(typeof age)
+
+
+// quyi
+// console.log(Math.floor(A))
+
+// yuqori
+// console.log(Math.ceil(A))
+
+// butun qism
+// console.log(Math.trunc(A))
+
+
+// console.log(Math.round(A))
+
+
+// let a;
+//  console.log (Math.trunc(Math.random(a)))
