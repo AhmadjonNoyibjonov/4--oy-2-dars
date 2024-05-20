25634389869a75b5bfacfcb46edc0fb59eff5772
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-05-20 15:43:42 +0500

    project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // Number
 // console.log(typeof age)
 
-
 // quyi
 // console.log(Math.floor(A))
 
@@ -11,9 +10,94 @@
 // butun qism
 // console.log(Math.trunc(A))
 
-
 // console.log(Math.round(A))
-
 
 // let a;
 //  console.log (Math.trunc(Math.random(a)))
+
+// 5- masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b sonini kiriting");
+// let c = +prompt("c sonini kiriting");
+// let d = +prompt("d sonini kiriting");
+// let musbat = 0;
+// let manfiy = 0;
+
+// if (a > 0) {
+//   musbat = musbat + 1;
+// } else  if (a < 0) {
+//   manfiy = manfiy + 1;
+// }
+
+// if (b % 2 == 1) {
+//   musbat = musbat + 1;
+// } else  if (b < 0){
+//   manfiy = manfiy + 1;
+// }
+
+// if (c % 2 == 1) {
+//   musbat = musbat + 1;
+// } else  if (c < 0){
+//   manfiy = manfiy + 1;
+// }
+
+// if (d % 2 == 1) {
+//   musbat = musbat + 1;
+// } else if (d < 0) {
+//   manfiy = manfiy + 1;
+// }
+
+// console.log(manfiy);
+// console.log(musbat);
+
+
+// 6-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+// let max = 0;
+
+// if (a > b) {
+//     max = a;
+// } else {
+//     max = b
+// }
+
+
+// 8-masala
+// let a = +prompt("a soni kiritilsin");
+// let b = +prompt("b soni kiritilsin");
+
+// let max = 0;
+// let min = 0;
+
+// if (a > b) {
+//   max = max + a;
+//   min = min + b;
+// } else if (b > a) {
+//   max = max + b;
+//   min = min + a;
+// }
+
+// console.log(max);
+// console.log(min);
+
+
+
+
+
+
+// 11- masala 
+// let A = +prompt("A soni kiritilsin");
+// let B = +prompt("B soni kiritilsin");
+
+// if (A == B) {
+//     A = 0;
+//     B = 0;
+// } else if (A > B) {
+//     A = A;
+//     B =A;
+// } else if (B > A) {
+//     A = B;
+//     B = B;
+// }
+
